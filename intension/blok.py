40ea3d491b8623e4add04ca1e8f8b1418f4d0771
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-29 21:52:34 -0500

    Add the blok class definition
    Fix linter errors
    Modify test for tensors

---
+++ intension/blok.py
@@ -0,0 +1,20 @@
+"""The Blok class definition"""
+
+
+class Blok:
+    """Blok is the simplest unit of the tensor. It takes in an array that describes coordinates."""
+
+    def __init__(self, coords):
+        self.i = coords
+        self.val = None
+        self.top = None
+        self.btm = None
+        self.lef = None
+        self.rgh = None
+
+    def __repr__(self):
+        return self.val
+
+    def set_value(self, value):
+        """Set the value of the Blok instance to be the input value."""
+        self.val = value
--- tests/test_blok.py
+++ tests/test_blok.py
@@ -2,5 +2,5 @@ from tkinter.messagebox import NO
 from intension import blok
 
 def test_blok():
-    b = blok.Blok(1, 1)
+    b = blok.Blok([1, 1])
     assert b.top == None
